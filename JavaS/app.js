a3efb36925aef83dd03b4bf06d44621c1b81b72b
farah Al-Saadi <93160583+Farah86@users.noreply.github.com>
2022-01-23 08:06:09 -0500

    Add files via upload

--- JavaS/app.js
+++ JavaS/app.js
@@ -23,3 +23,40 @@ cell.text(val);
   );
  });
 }
+// in here telling D3 to look for the #datetime id in the HTML tags 
+function handleClick() {
+  let date = d3.select("#datetime").property("value");
+  //This is the original data as imported from our data.js file
+  //setting the filteredData variable to our raw data, we're basically using it as a blank slate
+  let filteredData = tableData;
+  //If there is a date already set, then use that date as a filter. If not, then return the default data
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+};
+//call the buildtable function
+// Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+};
+//ied to the filter button we'll build on our webpage
+//we're linking our code directly to the filter button
+//we're telling D3 to execute our handleClick() function when the button with an id of filter-btn is clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+// Build the table when the page loads
+//call our buildTable function once more—this time using the original data we've imported
+buildTable(tableData);
+
+
+
+
+
+
+
+
+
+
+
+
